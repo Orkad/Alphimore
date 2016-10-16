abb8fe52c9f1c2f4094d259235eec27d8486c54d
Orkad <nicolas.gidon.43@gmail.com>
2016-10-16 16:32:22 +0200

    Progress bar now working (Vertical & Horrizontal)

--- Assets/Orkad/Scripts/ProgressBar.cs
+++ Assets/Orkad/Scripts/ProgressBar.cs
@@ -6,12 +6,19 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour {
 	public RectTransform emptyArea;
 	public RectTransform fillArea;
+	public RectTransform.Axis axis;
 	public float value;
 	public float minValue;
 	public float maxValue;
 
 	protected void Update () {
 		value = Mathf.Clamp(value,minValue,maxValue);
-		fillArea.sizeDelta = new Vector2(emptyArea.sizeDelta.x *MathExt.Ratio(value,minValue,maxValue),emptyArea.sizeDelta.y);
+		//fillArea.sizeDelta = new Vector2(emptyArea.sizeDelta.x *MathExt.Ratio(value,minValue,maxValue),emptyArea.sizeDelta.y);
+		if(axis == RectTransform.Axis.Horizontal)
+			fillArea.SetSizeWithCurrentAnchors(axis,MathExt.Ratio(value,minValue,maxValue)*emptyArea.sizeDelta.x);
+		else
+			fillArea.SetSizeWithCurrentAnchors(axis,MathExt.Ratio(value,minValue,maxValue)*emptyArea.sizeDelta.y);
 	}
+
+
 }
