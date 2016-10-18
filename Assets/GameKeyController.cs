15b57d59c1ae8eaab0672efcbfc9622a23164ed6
Orkad <nicolas.gidon.43@gmail.com>
2016-10-18 08:26:57 +0200

    Iventory panel + toogle on 'I'

--- Assets/GameKeyController.cs
+++ Assets/GameKeyController.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class GameKeyController : MonoBehaviour {
 	public KeyCode gameMenuKeyCode;
 	public Menu gameMenu;
+	public KeyCode inventoryKeyCode;
+	public AnnimatedUI_Fader inventory;
 
 	void Update(){
 		if (Input.GetKeyDown(gameMenuKeyCode)) {
 			gameMenu.Toogle ();
+		}
+		if (Input.GetKeyDown (inventoryKeyCode)) {
+			inventory.Toogle ();
 		}
 	}
 }
